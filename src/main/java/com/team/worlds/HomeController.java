34e6e910a01ad237f91344f131f02ab69649f3e0
Sotis Myo <yarunohosi@gmail.com>
2022-08-04 13:04:26 +0900

    [web Socket]

--- src/main/java/com/team/worlds/HomeController.java
+++ src/main/java/com/team/worlds/HomeController.java
@@ -1,10 +1,16 @@
 package com.team.worlds;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,9 +28,9 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(HttpServletRequest req) {
 		
-		uDAO.login(req);
+		//uDAO.login(req);
 		
-		return "index";
+		return "jw/testJSP/test";
 	}
 	@RequestMapping(value = "/home.go", method = RequestMethod.GET)
 	public String goHome(HttpServletRequest req) {
