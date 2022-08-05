e341fecb46a1cc2c922f5017d6dbd1d5c6c9d3d5
Sotis Myo <yarunohosi@gmail.com>
2022-08-05 13:04:40 +0900

    [08-05]맵추가

---
+++ src/main/webapp/resources/js/jw/map.js
@@ -0,0 +1,12 @@
+
+
+
+window.onload = function(){
+var countryElements = document.getElementById('countries').childNodes;
+    var countryCount = countryElements.length;
+for (var i = 0; i < countryCount; i++) {
+      countryElements[i].onclick = function() {
+        alert('You clicked on ' + this.getAttribute('data-name'));
+      }
+    }    	
+}
