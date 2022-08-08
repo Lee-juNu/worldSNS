6fbaa7870e6dc1d53538ba9e5d26b866322bccfb
Sotis Myo <yarunohosi@gmail.com>
2022-08-09 01:44:53 +0900

    [08-09][확대시 커서 중심 이동]

--- src/main/webapp/resources/js/jw/map.js
+++ src/main/webapp/resources/js/jw/map.js
@@ -6,56 +6,144 @@ var centerY  = null;
 var viewbox = null;
 var matrixGroup =null;
 
+
 // Create an SVGPoint for future math
 var pt = null;
 
 
+var mouseX=null;
+var mouseY=null;
 
+var onMouse = null;
+var prevMousePt = null; 
+
+var onClick = false;
+var prevScale = 1;
+
+
+var offsetPtX=0;
+var offsetPtY=0;
+
+var mouseOffsetX = 0;
+var mouseOffsetY = 0;
 
 
 window.onload = function(){
-var countryElements = document.getElementById('countries').childNodes;
-    var countryCount = countryElements.length;
+		
 
 
-	svg = document.getElementById('mapID');
-    viewbox = svg.getAttributeNS(null, "viewBox").split(" ");
-    centerX = parseFloat(viewbox[2]) / 2;
-    centerY = parseFloat(viewbox[3]) / 2;
-    matrixGroup = svg.getElementById("countries");
-    pt = svg.createSVGPoint();  
-  var newMatrix = "matrix(" +  transformMatrix.join(' ') + ")";
-  matrixGroup.setAttributeNS(null, "transform", newMatrix);
+	addCountrySelectEvent();
+	mapMatrixInit();
+	addMapDivEvnet();
+
+	svg.addEventListener('mousemove',function(evt){
+  		var loc = cursorPoint(evt);
+		onMouse = loc;
+		if(onClick)
+		{
+			MapMove();			
+		}
+		
+		
+	},false);    	
+	
+	document.addEventListener("wheel", function (e) {
+	
+	if (e.deltaY > 0) {
+			zoom(-0.2,e);    		 
+  		} else {
+			zoom(0.25,e);
+  		}
+	});
+}
 
 
-for (var i = 0; i < countryCount; i++) {
+function addCountrySelectEvent()
+{
+	var countryElements = document.getElementById('countries').childNodes;
+    	var countryCount = countryElements.length;
+    	for (var i = 0; i < countryCount; i++) {
       countryElements[i].onclick = function() {
         alert('You clicked on ' + this.getAttribute('data-name'));
       }
     }
-
-svg.addEventListener('mousemove',function(evt){
-  var loc = cursorPoint(evt);
-},false);    	
 }
 
 
 
-document.addEventListener("wheel", function (e) {
-  if (e.deltaY > 0) {
-   		 zoom(1.25);
-  } else {
-   		 zoom(0.8);
-  }
-});
+function addMapDivEvnet()
+{
+	var mapDiv = document.getElementById("tempMapDiv");
 
-function zoom(scale) {
-  for (var i = 0; i < 4; i++) {
-    transformMatrix[i] *= scale;
-  }
-  transformMatrix[4] += (1 - scale) * centerX;
-  transformMatrix[5] += (1 - scale) * centerY;
-		        
+	mapDiv.addEventListener('mousedown', (event) => {
+  			mapMouseDown(event);
+		});
+		document.addEventListener('mouseup', (event) => {
+  			mapMouseUp();
+		});
+}
+
+function mapMatrixInit()
+{
+		svg = document.getElementById('mapID');
+    	viewbox = svg.getAttributeNS(null, "viewBox").split(" ");
+    	centerX = parseFloat(viewbox[2]) / 2;
+    	centerY = parseFloat(viewbox[3]) / 2;
+    	matrixGroup = svg.getElementById("countries");
+    	pt = svg.createSVGPoint(); 
+ 		var newMatrix = "matrix(" +  transformMatrix.join(' ') + ")";
+  		matrixGroup.setAttributeNS(null, "transform", newMatrix);
+
+}
+
+
+
+function MapMove()
+{
+	var changePtX = prevMousePt.x -onMouse.x;
+	var changePtY = prevMousePt.y -onMouse.y;
+	
+	offsetPtX+=(changePtX);
+	offsetPtY+=(changePtY);
+	pan(-changePtX,-changePtY);
+	prevMousePt.x =onMouse.x;
+	prevMousePt.y =onMouse.y;
+}
+
+
+ var prevCursor= null;
+
+function zoom(scale,event) {
+
+	var prevScale =  transformMatrix[0];	
+	
+	console.log(transformMatrix);	
+    
+	if(transformMatrix[0]+scale<1)
+	{
+		transformMatrix[0]= 1;
+		transformMatrix[3]= 1;
+	}
+	else if(transformMatrix[0]+scale>8)
+	{
+		transformMatrix[0]= 8;
+		transformMatrix[3]= 8;
+	}
+	else{
+		transformMatrix[0] += scale;
+    	transformMatrix[3] += scale;
+	}
+	
+
+	
+
+	console.log(transformMatrix[0]-prevScale);	
+  	transformMatrix[4] += (prevScale -transformMatrix[0]) * (cursorPoint(event).x + offsetPtX);
+  	transformMatrix[5] += (prevScale -transformMatrix[0]) * (cursorPoint(event).y + offsetPtY);
+	console.log(transformMatrix);	
+
+
+
   var newMatrix = "matrix(" +  transformMatrix.join(' ') + ")";
   matrixGroup.setAttributeNS(null, "transform", newMatrix);
 }
@@ -66,6 +154,8 @@ function pan(dx, dy) {
             
   var newMatrix = "matrix(" +  transformMatrix.join(' ') + ")";
   matrixGroup.setAttributeNS(null, "transform", newMatrix);
+	console.log(transformMatrix);	
+
 }
 
 
@@ -75,9 +165,21 @@ function pan(dx, dy) {
 // Get point in global SVG space
 function cursorPoint(evt){
   pt.x = evt.clientX; pt.y = evt.clientY;
-console.log(pt.x);
-console.log(pt.y);
   return pt.matrixTransform(svg.getScreenCTM().inverse());
+}
+
+function mapMouseDown(event)  {
+  	onClick=true;
+	prevMousePt= cursorPoint(event);
+	
+	
+	console.log(onClick);
+	console.log(Math.floor(cursorPoint(event).x));
+	console.log(Math.floor(cursorPoint(event).y));
+}
+function mapMouseUp()  {
+  	onClick=false;
+	console.log(onClick);
 }
 
 
