c578b15faabd96b2994561b3c43dfbc50f0285c6
jys <rnfma6469@gmail.com>
2022-09-01 12:58:44 +0900

    fix update index

--- target/classes/com/team/worlds/db.sql
+++ target/classes/com/team/worlds/db.sql
@@ -29,10 +29,21 @@ select * from  userTbl;
 	select * from messageTbl where msg_RoomNum = 'CR215' order by msg_sendTime
 	select * from RoomMemberTbl
 	select * from messageTbl
+	select rm_lastindex from ROOMMEMBERTBL where rm_userid = 'admin'
 	select * from chatroomTbl
-select rm_roomnum from RoomMemberTbl where rm_userid = '5'
-	select * from chatroomTbl where cr_num= (select rm_roomnum from RoomMemberTbl where rm_userid like like '%5%') 
-	delete from chatroomTbl
-	delete from MESSAGETBL
+select rm_roomnum from RoomMemberTbl where rm_userid like '%9%'
+	select * from chatroomTbl where cr_num in (select rm_roomnum from RoomMemberTbl where rm_userid like '%5%') 
 insert into messageTbl values('0', 'CR216', 'admin', sysdate, '0', 'cont', 'cont')	
 insert into userTbl values('yorunohosi','team802!@$','yorunohosi','01089854474','이준우','yorunohosi@naver.com',sysdate);
+
+select max(msg_index) from messageTbl where msg_roomnum ='CR477'
+select msg_index from MESSAGETBL where msg_index =(
+select distinct msg_roomnum, msg_index from MESSAGETBL where msg_index = (
+select msg_index, msg_roomnum from MESSAGETBL where msg_roomnum in (select distinct msg_roomnum from MESSAGETBL)
+	
+select max(msg_index) from MESSAGETBL group by msg_roomnum
+select rm_lastindex from RoomMemberTbl where rm_userid = 'admin' 
+select msg_roomnum, msg_index from MESSAGETBL
+
+
+select max(msg_index) from messageTbl where msg_roomnum in (select rm_roomNum from roomMemberTbl where rm_roomNum='CR455' )
